667908cf1bc5e0ff2b60d51834bcd9f1d6962bf7
John Humbert Duran <johnhumbertd@gmail.com>
2020-01-27 09:43:55 +0800

    first commit

---
+++ bins/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, maximum-scale=1, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="title" content="Student Performance Prediction">
+    <meta property="og:title" content="Student Performance Prediction">
+    <meta name="robots" content="index,follow">
+    <meta name="MobileOptimized" content="width" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta name="description" content="StudentPerformancePrediction">
+    <meta name="keywords" content="Student,Performance,Prediction">
+    <!-- <meta property="og:image" content="me.JPG"> -->
+
+    <!-- <link rel="icon" type="image/png" href="public/images/logo.png"> -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> --> <!-- Bootstrap4 for CDN (Content Delivery Network) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap-4.4.1/css/bootstrap.min.css"> <!-- Bootstrap4 for offline -->
+
+    <link rel="stylesheet" href="public/styles/styles.css">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" integrity="sha384-rtJEYb85SiYWgfpCr0jn174XgJTn4rptSOQsMroFBPQSGLdOC5IbubP6lJ35qoM9" crossorigin="anonymous">
+    
+    <title>Student Performance Prediction</title>
+
+    </head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include("bins/header.php");
+include("bins/nav.php");
+// include("piechart.php");
+?>
+
+<div class="">
+<?php
+include("piechart.php");
+?>
+</div>
+
+<?php
+include("bins/footer.php");
+?>
